98a50d46e78ab3d0e2650bd4fd5b26a7241edbdb
Gray754 <lgc9312@gmail.com>
2019-02-05 15:16:38 -0600

    finished state-4

--- stage-3/src/components/StoreFront/StoreFront.js
+++ stage-3/src/components/StoreFront/StoreFront.js
@@ -11,22 +11,30 @@ class StoreFront extends Component {
 
     render() {
         console.log(this.props.products);
-        let productDisplay = this.props.products.map((element, index) => {
+        if(this.props.products && this.props.products[0]){
+            let productDisplay = this.props.products.map((element, index) => {
+                return (
+                    <div className="product-container" key={index}>
+                        <h2>{element.title}</h2>
+                        <img src={element.image} alt="" />
+                        <h2>{element.desc}</h2>
+                        <h3>{"$" + element.price + ".00"}</h3>
+                        <button onClick={() => this.props.addToShoppingCart(element)}>Purchase!</button>
+                    </div>
+                )
+            })
             return (
-                <div className="product-container" key={index}>
-                    <h2>{element.title}</h2>
-                    <img src={element.image} alt="" />
-                    <h2>{element.desc}</h2>
-                    <h3>{"$" + element.price + ".00"}</h3>
-                    <button onClick={() => this.props.addToShoppingCart(element)}>Purchase!</button>
+                <div className="storefront-container">
+                    {productDisplay}
                 </div>
             )
-        })
-        return (
-            <div className="storefront-container">
-                {productDisplay}
-            </div>
-        )
+        }else{
+            return(
+                <div>
+                    Loading...
+                </div>
+            )
+        }
     }
 }
 
